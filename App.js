d0f252edd535cd0a4c52827b044fd782d943ef55
jayweb007 <jayweb007@gmail.com>
2022-02-18 16:48:59 -0600

    commit on master

--- App.js
+++ App.js
@@ -1,11 +1,19 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
+import Amplify from "aws-amplify";
+import { withAuthenticator } from "aws-amplify-react-native";
+
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import Root from "./navigations/Root";
 import { getStatusBarHeight } from "react-native-status-bar-height";
 import PlayerWidget from "./components/PlayerWidget";
+import awsconfig from "./src/aws-exports";
 
+//
+Amplify.configure(awsconfig);
 const statusBarHeight = getStatusBarHeight();
+
+//
 const App = () => {
   return (
     <SafeAreaProvider style={styles.container}>
@@ -16,7 +24,7 @@ const App = () => {
   );
 };
 
-export default App;
+export default withAuthenticator(App);
 
 const styles = StyleSheet.create({
   container: {
---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,19 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "spotifyclone80ce0089": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    },
+    "api": {
+        "spotifyclone": {
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
+    }
+}
